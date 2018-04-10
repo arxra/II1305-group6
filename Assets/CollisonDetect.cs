808c985781e8ecb50336fee805045cb29e1a47bd
Liam Carter <liamcarter9182@gmail.com>
2018-04-10 08:51:24 +0200

    player scripts
    added script for jump and for simple collision detection

---
+++ Assets/CollisonDetect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisonDetect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "player")
+        {
+            Debug.Break();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
