eaa19fe460b7470b491d9e036050dea992d399f3
Per Fahlander <perfah@kth.se>
2018-04-10 16:14:32 +0200

    - Solves #6 - Random obstacle spawning & despawning \n - Simple controls

---
+++ Assets/ObstacleRemover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleRemover : MonoBehaviour {
+	public string ObstacleTag = "Obstacle";
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag.Contains(ObstacleTag))
+        	Destroy(other.gameObject);
+    }
+}
