5aaa9a67a240a952c849e03234b06f37db1a27cb
Aron <judoaron@hotmail.com>
2018-04-11 16:52:31 +0200

    Basic score
    Score is dependant on the magnitue of the road movement.

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+  [Tooltip("The current players score")]
+    public int score;
+
+  //Read the speed from the WorldMover script to calclate the points.
+  private WorldMover mv; 
+  // Use this for initialization
+  void Start () {
+mv : WorldMover = GetComponent(WorldMover);
+    score = 0;
+  }
+
+  // Update is called once per frame
+  void Update () {
+    score += WorldMover.Magnitude/100;
+  }
+}
