4ec0115fda5475fc42cc17379afa46d3abf6694e
Unknown <joarek@kth.se>
2018-04-11 10:35:51 +0200

    Pickups
    Created a rudamentary pickup system :)

---
+++ Assets/Scripts/pickup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour {
+	public string countText;
+	private int count;
+
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		SetCountText ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other) 
+	{
+		if (other.collider.gameObject.CompareTag ("Collectable"))
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText () {
+		countText = "Count: " + count.ToString(); 
+	}
+
+	void OnGUI () {
+		GUI.Box (new Rect (0, 50, Screen.width, Screen.height - 50), countText);
+	}
+}
