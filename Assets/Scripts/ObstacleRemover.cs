031420021621311bbb79a2bcd5dfb1e721d47009
Aron <judoaron@hotmail.com>
2018-04-17 14:43:16 +0200

    Despawns a second target
    Despawner now also despawns things with the tag "Pick Up"

--- Assets/Scripts/ObstacleRemover.cs
+++ Assets/Scripts/ObstacleRemover.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class ObstacleRemover : MonoBehaviour {
 	public string ObstacleTag = "Obstacle";
+  public string ObstacleTag2 = "Pick Up";
 
 	void OnTriggerEnter(Collider other) {
-		if (other.tag.Contains(ObstacleTag))
+		if (other.tag.Contains(ObstacleTag) || other.tag.Contains(ObstacleTag2))
         	Destroy(other.gameObject);
     }
 }
