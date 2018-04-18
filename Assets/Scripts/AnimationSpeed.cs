4391044228d41b928dc98f71b3b55fa63ae5eefe
Unknown <joarek@kth.se>
2018-04-18 09:39:20 +0200

    Auto stash before merge of "Develop" and "origin/Develop"

---
+++ Assets/Scripts/AnimationSpeed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSpeed : MonoBehaviour {
+	public float speedScaler;
+
+	Animator anim;
+	WorldMover worldMoverReference;
+	GameObject worldMoverFinder;
+	float speed;
+
+	// Use this for initialization
+	void Start () {
+		worldMoverFinder = GameObject.Find("Road");
+		anim = GetComponent<Animator> ();
+		worldMoverReference = worldMoverFinder.GetComponent<WorldMover> ();
+		speed = 0;
+		speedScaler = 30.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		speed = worldMoverReference.GetCurrentSpeed() / speedScaler;
+		anim.speed = 1 + speed;
+		Debug.Log ("speed " + speed + "anim speed " + anim.speed) ;
+	}
+}
