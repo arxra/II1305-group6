4329bf9e4015e2f2b4038aa18ceda0792ad22303
Per Fahlander <perfah@kth.se>
2018-05-02 10:33:27 +0200

    Update for ObstacleCreator accordingly

--- Assets/Scripts/ObstacleCreator.cs
+++ Assets/Scripts/ObstacleCreator.cs
@@ -4,87 +4,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstacleCreator : MonoBehaviour {
-
-	[System.Serializable]
-	public class SpawnSession {
-		[Tooltip("A pointer to the candidate gameobject you want to override the spawn settings for")]
-		public GameObject Candidate;
-		
-		[Tooltip("An indicator to how often this gameobject should spawn in relation to other candidates")]
-		public int Popularity;
-
-		[Tooltip("The number of remaining instances this candidate should spawn (-1 means infinity)")]
-		public int RemainingInstances;
-
-		[Tooltip("A list of lanes you don't want this candidate to spawn in")]
-		public List<int> ExcludeLanes;
-
-		public SpawnSession(GameObject candidate, int popularity = 10, int remainingInstances = -1) {
-			Candidate = candidate;
-			Popularity = popularity;
-			RemainingInstances = remainingInstances; 
-			ExcludeLanes = new List<int>();
-		}
-
-		public bool InstantiateAt(Vector3 position) {
-			if (RemainingInstances == 0)
-				return false;
-			else if(RemainingInstances > 0)
-				RemainingInstances--;
-
-			Object.Instantiate(Candidate, position, Candidate.transform.rotation);
-			return true;
-		}
-
-		public Vector3 GetSuitableSpawnPoint(Transform spawnPoints, ref int lastLane) {
-			ExcludeLanes.Add(lastLane);
-			var range = Enumerable.Range(0, spawnPoints.childCount).Where(v => !ExcludeLanes.Contains(v));
-			ExcludeLanes.Remove(lastLane);
-
-			if (range.Any()){
-				lastLane = range.ElementAt(Random.Range(0, range.Count()));
-
-				return (
-					spawnPoints.GetChild(lastLane).position + 
-					Vector3.up * Candidate.transform.position.y
-				);
-			}
-			else
-				throw new UnityException("You fucked up! Too many excluded lanes :(");
-		}
-	} 
-
 	[Tooltip("The time between each obstacle spawn [seconds]")]
 	public float AutoSpawnInterval = 1;
-	
-	[Tooltip("Overrides the default spawn settings for a candidate")]
-	public List<SpawnSession> SpawnOverrides = new List<SpawnSession>();
+
+	private List<SpawnPolicy> policies;
 
 	private int totalPopularity;
 	private float timeout;
 	private int lastLane;
 
-	public ObstacleCreator() {
-		SpawnOverrides = new List<SpawnSession>() {
-			new SpawnSession(null)
-		};
-	}
-
 	// Use this for initialization
 	void Start () {
+		policies = new List<SpawnPolicy>();
 		timeout = AutoSpawnInterval;	
 		totalPopularity = 0;	
 		lastLane = -1;
 
 		foreach(Transform transf in transform.Find("Candidates")) {
-			var session = SpawnOverrides.Find(s => s.Candidate.Equals(transf.gameObject));
-			
-			if (session == null) {
-				session = new SpawnSession(transf.gameObject);
-				SpawnOverrides.Add(session);
-			}
+			var policy = transf.GetComponent<SpawnPolicy>();
 
-			totalPopularity += session.Popularity;
+			if (policy == null)
+				policy = new SpawnPolicy(transf.gameObject);
+
+			totalPopularity += policy.Popularity;
 		}
 	}
 	
@@ -99,8 +41,8 @@ public class ObstacleCreator : MonoBehaviour {
 
 		var session = SpawnNow();
 		if (session != null){
-			if (session.Candidate == null || !session.InstantiateAt(session.GetSuitableSpawnPoint(transform.Find("Spawn Points"), ref lastLane))){
-				SpawnOverrides.Remove(session);
+			if (!session.InstantiateAt(session.GetSuitableSpawnPoint(transform.Find("Spawn Points"), ref lastLane))){
+				policies.Remove(session);
 				totalPopularity -= session.Popularity;
 			}
 		}
@@ -110,13 +52,13 @@ public class ObstacleCreator : MonoBehaviour {
 		}
 	}
 
-	public SpawnSession SpawnNow() {
+	public SpawnPolicy SpawnNow() {
 		int rand = Random.Range(0, totalPopularity);
 		int pos = 0;
 
-		foreach(SpawnSession session in SpawnOverrides) {
-			if (rand >= pos & rand < (pos += session.Popularity))
-				return session;
+		foreach(SpawnPolicy policy in policies) {
+			if (rand >= pos & rand < (pos += policy.Popularity))
+				return policy;
 		}
 
 		return null;
