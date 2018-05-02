cd36d7781ed8d845b06570447b9f913ca1dcddf3
Aron <judoaron@hotmail.com>
2018-05-02 14:54:20 +0200

    Upgrades!
    currency implemented into a upgrade module.
    TODO, actuall upgrades

---
+++ Assets/Scripts/Upgrades.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Upgrades : MonoBehaviour {
+
+  [Tooltip("Food currency. Public ONLY for testing purposes.")]
+    public int _currency;
+  public GameOverScreen _gos;
+  public Score _sc;
+
+  public void Start() {
+  }
+
+  //How much monies?
+  public int CurrentCurrency() {
+    return _currency;
+  }
+
+  //make it rain!
+  public bool SpendMoney(int toBeSpent) {
+    if(toBeSpent <= _currency) {
+      _currency -= toBeSpent;
+      return true;
+    }
+    return false;
+  }
+
+  public void Update() {
+    //IF kill, then add current runs currency collection to the main currency
+    if (! _gos.GetComponent<GameOverScreen>().alive){
+      _currency += _sc.GetComponent<Score>().RunsFood();
+      PlayerPrefs.SetInt("currency", (_currency + PlayerPrefs.GetInt("currency")));
+    }
+  }
+}
