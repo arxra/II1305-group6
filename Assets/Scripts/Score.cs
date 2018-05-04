e2b96e58f7cadef27b3a45f2cf636badb5fea1b4
Aron <judoaron@hotmail.com>
2018-05-04 10:36:08 +0200

    Basic Upgrades framework
    Upgrades are now not erroring
    
    Less error, more clean code

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -5,14 +5,26 @@ using System.Collections.Generic;
 public class Score : MonoBehaviour {
 
   [Tooltip("The current players score")]
+<<<<<<< HEAD
     public int score;
       public Text highScore;
       public Text text;
+=======
+    public int score;
+
+  [Tooltip("Insert upgrade module here")]
+    public Upgrades _upgrades;
+  public Animator anim;
+  private bool TheBoolThatTellsIfTheAnimationForANewHighScoreHasBeenPlayed = false;
+  public Text highScore;
+  public Text text;
+  public Text MultiText;
+>>>>>>> 7ea580f... Basic Upgrades framework
   public GameOverScreen go;
   public bool alive;
   public float multiplier;
   private Dictionary<int, MulStruct> _multis = new Dictionary<int, MulStruct>();
-  private WorldMover _mv = new WorldMover();
+  private WorldMover _mv;
 
   private float _foodFactor;
 
@@ -26,21 +38,41 @@ public class Score : MonoBehaviour {
     public float _mult;
     public int _location;
   }
+<<<<<<< HEAD
   // Use this for initialization
+=======
+
+>>>>>>> 7ea580f... Basic Upgrades framework
   void Start () {
     _mv = GameObject.Find("WorldMover").GetComponent<WorldMover>();
     alive = true;
     score = 0;
     highScore.text = "High Score: " + PlayerPrefs.GetInt("highScore");
-      updateText();
+    updateText();
   }
 
+<<<<<<< HEAD
   // Update is called once per frame
   void Update () {
     alive = go.GetComponent<GameOverScreen>().alive;
     float scoreUpdate = 0;
     //Tick multiplier
+=======
+
+  void Update () {
+    alive = go.GetComponent<GameOverScreen>().alive;
+    float scoreUpdate = 0;
+
+    //Tick multiplier  (Multiply by multipliers) 
+>>>>>>> 7ea580f... Basic Upgrades framework
     multiplier = 1f;
+
+    foreach(Upgrades.Upgrade id in _upgrades.CurrentUpgrades())
+      if(id._name.Equals("multiplier")){ 
+        multiplier += id._level;
+      }
+
+
     foreach (MulStruct mul in new Dictionary<int, MulStruct>(_multis).Values) {
       mul._time -= Time.deltaTime;
       if(mul._time <=0) {
@@ -56,6 +88,7 @@ public class Score : MonoBehaviour {
         scoreUpdate += _mv.GetCurrentSpeed() * Time.deltaTime * multiplier;
           score += Mathf.RoundToInt(scoreUpdate);
       }
+<<<<<<< HEAD
     int oldHighScore = PlayerPrefs.GetInt("highScore");
       if (score > oldHighScore)
         PlayerPrefs.SetInt("highScore", score);
@@ -64,10 +97,30 @@ public class Score : MonoBehaviour {
           
           
           updateText();
+=======
+
+    //Update highscore
+    int oldHighScore = PlayerPrefs.GetInt("highScore");
+      if (score > oldHighScore)
+      {
+        PlayerPrefs.SetInt("highScore", score);
+          if (!TheBoolThatTellsIfTheAnimationForANewHighScoreHasBeenPlayed)
+          {
+            anim.SetTrigger("high");
+              TheBoolThatTellsIfTheAnimationForANewHighScoreHasBeenPlayed = true; 
+          }
+      }
+    highScore.text = "High Score: " + PlayerPrefs.GetInt("highScore");
+      updateText();
+>>>>>>> 7ea580f... Basic Upgrades framework
   }
 
   void updateText()
   {
+<<<<<<< HEAD
+=======
+    MultiText.text = "X" + multiplier;
+>>>>>>> 7ea580f... Basic Upgrades framework
     text.text = "Score : "+ score ;
   }
 
@@ -77,6 +130,17 @@ public class Score : MonoBehaviour {
     return tmp;
   }
 
+<<<<<<< HEAD
+=======
+  //Use only when game's lost
+  public int RunsFood() {
+    int tmep = _totalFoodForRun;
+    _totalFoodForRun = 0;
+    return tmep;
+  }
+
+  //Pickup collectable, add value and destroy when done
+>>>>>>> 7ea580f... Basic Upgrades framework
   void OnTriggerEnter(Collider col){
     if(ObjectFilter.EntityHasTags(col.gameObject ,ObjectFilter.Tag.Collectable)){
       GameObject pckup = col.gameObject;
---
+++ Assets/Scripts/Upgrades.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+//Upgrade behaviour
+public class Upgrades : MonoBehaviour {
+
+  [Tooltip("Food currency. Public ONLY for testing purposes.")]
+    public int _currency;
+  public GameOverScreen _gos;
+  public Score _sc;
+  public List<Upgrade> _ownedUpgrades = new List<Upgrade>();
+
+
+  //Upgrades
+  private static string[] _ups = {
+    "multiplier",
+    "maximum_speed",
+    "boost_speed",
+    "multiplier_speed",
+    "more_valuable_foods",
+    "godmode_timer"
+  };
+
+
+  public void Start() {
+    foreach (string up in _ups){
+      _ownedUpgrades.Add(new Upgrade(up));
+    }
+  }
+
+
+  //How much monies?
+  public int CurrentCurrency() {
+    return _currency;
+  }
+
+
+  //make it rain! (Check if money can be spent)
+  public bool SpendMoney(int toBeSpent) {
+    if(toBeSpent <= _currency) {
+      _currency -= toBeSpent;
+      return true;
+    }
+    return false;
+  }
+
+
+  public List<Upgrade> CurrentUpgrades() {
+    return _ownedUpgrades;
+  }
+
+
+  public void Update() {
+    //IF kill, then add current runs currency collection to the main currency
+    if (! _gos.GetComponent<GameOverScreen>().alive)
+      _currency += _sc.GetComponent<Score>().RunsFood();
+      PlayerPrefs.SetInt("currency", (_currency + PlayerPrefs.GetInt("currency")));
+      PlayerPrefs.Save();
+  }
+
+
+  public class Upgrade {
+    public Upgrade(string na){
+      _name = na;
+      _level = PlayerPrefs.GetInt(na);
+    //Increment the cost of stuff exponatially
+      _cost = Mathf.RoundToInt(Mathf.Pow(100, (0.2f * _level + 1)));
+    }
+    public string _name;
+    public int _level;
+    public int _cost;
+  }
+}
