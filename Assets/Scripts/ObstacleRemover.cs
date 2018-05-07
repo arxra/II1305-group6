5f50a7afc77ea2f1b15c5fad8cba103c1423869f
Aron <judoaron@hotmail.com>
2018-05-07 08:30:45 +0200

    Now removes all objects instead of some

--- Assets/Scripts/ObstacleRemover.cs
+++ Assets/Scripts/ObstacleRemover.cs
@@ -5,8 +5,9 @@ public class ObstacleRemover : MonoBehaviour {
 	public ObjectFilter.Tag _filterTag = ObjectFilter.Tag.Foreground;
 
 	void OnTriggerEnter(Collider other) {
-		var enemy = ObjectFilter.RelativesHasTags(other.gameObject, ObjectFilter.Tag.Foreground);
+		//var enemy = ObjectFilter.RelativesHasTags(other.gameObject, ObjectFilter.Tag.Foreground);
 
+    var enemy = other.gameObject;
 		if (enemy != null)	
         	Object.Destroy(enemy, 10f);
     }
