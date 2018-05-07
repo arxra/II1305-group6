78de8e17152c5a06f224b0370be298bb7aa18169
Aron <judoaron@hotmail.com>
2018-05-07 10:52:27 +0200

    Upgradeable Godmode
    Godmode is now extended by 1 second per level of godmode upgrade

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -1,87 +1,87 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class GameOverScreen : MonoBehaviour
 {
 
-    public GameOverManager go;
-    private GameObject[] Obstical;
-    GameObject[] pauseObjects;
-    public bool alive = true;
-	public bool godMode;
-    Animator anim;
-	private float immortalityTimer;
+  public GameOverManager go;
+  private GameObject[] Obstical;
+  GameObject[] pauseObjects;
+  public bool alive = true;
+  public bool godMode;
+  Animator anim;
+  private float immortalityTimer;
+  public Upgrades ups;
 
-    private WorldMover worldMover;
-    
-    void Start()
+  private WorldMover worldMover;
+
+  void Start()
+  {
+    Time.timeScale = 1;
+    pauseObjects = GameObject.FindGameObjectsWithTag("Pause");
+    alive = true;
+    godMode = false;
+    hidePause();
+    worldMover = GameObject.Find("WorldMover").GetComponent<WorldMover>();
+    immortalityTimer = 3.0f;
+
+    foreach(Upgrades.Upgrade t in ups.CurrentUpgrades())
+      if(t.IsMyName("godmode_timer"))
+        immortalityTimer += t.Level();
+  }
+
+  void Update()
+  {
+    if (Input.GetKeyDown("p"))
     {
-        
+      if (Time.timeScale == 1 && alive == true)
+      {
+        Time.timeScale = 0;
+        showPause();
+      }
+      else if (Time.timeScale == 0 && alive == true)
+      {
         Time.timeScale = 1;
-        pauseObjects = GameObject.FindGameObjectsWithTag("Pause");
-        alive = true;
-		godMode = false;
         hidePause();
-        worldMover = GameObject.Find("WorldMover").GetComponent<WorldMover>();
-		immortalityTimer = 3.0f;
-    }
+      }
 
-    void Update()
+      worldMover.IsKill = false;
+    }
+  }
+  private void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.tag.Equals("Obstacle"))
     {
-        if (Input.GetKeyDown("p"))
-        {
-            if (Time.timeScale == 1 && alive == true)
-            {
-                Time.timeScale = 0;
-                showPause();
-            }
-            else if (Time.timeScale == 0 && alive == true)
-            {
-                Time.timeScale = 1;
-                hidePause();
-            }
-
-            worldMover.IsKill = false;
-        }
+      if (!godMode) {
+        alive = false;
+        go.GetComponent<GameOverManager> ().GameOver ();
+        worldMover.IsKill = true;
+      } else {
+        unlimitedPower((collision.transform.parent.gameObject));
+      }
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag.Equals("Obstacle"))
-        {
-			if (!godMode) {
-				alive = false;
-				go.GetComponent<GameOverManager> ().GameOver ();
-				worldMover.IsKill = true;
-			} else {
-				unlimitedPower((collision.transform.parent.gameObject));
-			}
-        }
-    }
+  }
 
-	public void unlimitedPower(GameObject toDestroy) {
-		Destroy (toDestroy);
-	}
+  public void unlimitedPower(GameObject toDestroy) {
+    Destroy (toDestroy);
+  }
 
-	public void activateGodMode() {
-		godMode = true;
-		Invoke ("deactivateGodMode", immortalityTimer);
-	}
-	public void deactivateGodMode() {
-		godMode = false;
-	}
+  public void activateGodMode() {
+    godMode = true;
+    Invoke ("deactivateGodMode", immortalityTimer);
+  }
+  public void deactivateGodMode() {
+    godMode = false;
+  }
 
 
-    public void hidePause()
-    {
-        foreach (GameObject g in pauseObjects)
-            g.SetActive(false);
-    }
-    public void showPause()
-    {
-        foreach (GameObject g in pauseObjects)
-            g.SetActive(true);
-    }
+  public void hidePause()
+  {
+    foreach (GameObject g in pauseObjects)
+      g.SetActive(false);
+  }
+  public void showPause()
+  {
+    foreach (GameObject g in pauseObjects)
+      g.SetActive(true);
+  }
 }
