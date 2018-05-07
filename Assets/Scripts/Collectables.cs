2528cfaec0e85f4c88a097aa64d01445f484de82
Aron <judoaron@hotmail.com>
2018-05-07 11:39:44 +0200

    Collectable coins!

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -14,6 +14,15 @@ public class Collectables : MonoBehaviour {
   [Tooltip("Food factor, for boost and currency")]
     public int _sizeMultiplier;
 
+  public Upgrades up;
+
+  public void Start() {
+    foreach (Upgrades.Upgrade t in up.CurrentUpgrades()){
+      if(t.IsMyName("multiplier_duration"))
+        _time += t.Level() * 5;
+    }
+  }
+
   void Update() {
     // Rotate the object around its local X axis at 1 degree per second
     transform.Rotate(Vector3.up * Time.deltaTime * 75);
